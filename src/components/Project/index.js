5c2fbc5f7a833850c9eb92594ded01adb24edd4f
Maykon Capellari <maykonluiscapellari@gmail.com>
2019-06-16 12:41:47 -0300

    string interolate

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 export default function Project({ project }) {
-  const { name, html_url, description } = project;
+  const { name, full_name, html_url, description } = project;
+  const fullname = `${name} - ${full_name}`;
   return (
     <li>
-      <h3>{name}</h3>
+      <h3>{fullname}</h3>
       <p>{description}</p>
       <a href={html_url}>{html_url}</a>
     </li>
